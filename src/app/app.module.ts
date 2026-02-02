512d278740ca33fadbd52d04dcc15f54f78d0e94
Med Rayen Zaafouri <mohamedrayenzaafouri@gmail.com>
2026-02-02 10:06:18 +0100

    feat bootstrapped header, footer and suggestion list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/list-suggestion/list-suggestion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-suggestion',
+  templateUrl: './list-suggestion.component.html',
+  styleUrl: './list-suggestion.component.css'
+})
+export class ListSuggestionComponent {
+
+}
